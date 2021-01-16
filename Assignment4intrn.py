8dcd8bf3d677d834656c2c8cc5c281567826a8f2
Junaid <59776318+Junaidahbhat@users.noreply.github.com>
2021-01-16 13:56:26 +0530

    Add files via upload

---
+++ Assignment4intrn.py
@@ -0,0 +1,13 @@
+import turtle
+#scale used 1 unit of length in actual triangle=50 units of length in python
+
+turtle.forward(600)
+turtle.left(157.4)
+
+turtle.forward(650)
+turtle.left(112.6)
+
+turtle.forward(250)
+
+turtle.hideturtle()
+turtle.done()
