c9edefc56e99d2f2917826936964a48dbdad6c47
Junaid <59776318+Junaidahbhat@users.noreply.github.com>
2021-01-16 14:18:20 +0530

    Update Assignment4intrn.py

--- Assignment4intrn.py
+++ Assignment4intrn.py
@@ -1,13 +1,13 @@
 import turtle
-#scale used 1 unit of length in actual triangle=50 units of length in python
+#scale used 1 unit of length in actual triangle=20  units of length in python
 
-turtle.forward(600)
+turtle.forward(240)
 turtle.left(157.4)
 
-turtle.forward(650)
+turtle.forward(260)
 turtle.left(112.6)
 
-turtle.forward(250)
+turtle.forward(100)
 
 turtle.hideturtle()
 turtle.done()
